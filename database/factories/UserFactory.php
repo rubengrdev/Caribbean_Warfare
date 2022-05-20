11500133739f1adf2b9120a688e1647120f42c6e
ruben <rubengorodri@gmail.com>
2022-05-20 17:59:18 +0200

    factory users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /*
@@ -19,10 +20,13 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->unique()->username(),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role_id'=>1,
+        'region_id'=>Int::random(3),
+        'password' => Hash::make('1234'),
         'remember_token' => Str::random(10),
+
     ];
 });
