213207bcb9aeaee040580c7271af7144fca988c8
ruben <rubengorodri@gmail.com>
2022-05-23 15:46:06 +0200

    restore password css fix

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
 Route::get('/shop', 'ShopController@index')->name('shop');
+
 Route::get('leaderboard', 'LeaderboardController@index')->name('leaderboard');
 Route::get('leaderboard', 'LeaderboardController@getTop')->name('getTop');
