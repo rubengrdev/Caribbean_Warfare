78db1ce995521185d2a9543e792f72b9dbd25f6e
ruben <rubengorodri@gmail.com>
2022-05-30 18:36:41 +0200

    edit product view and controller

--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app_black')
-
 @section('content')
 <div class="show-header">
     <div class="margin-header">
@@ -10,7 +9,9 @@
     <span class="bigpan"></span>
     @if (Auth::user()->role_id == 2)
     <div class="options-cart">
+        <a href="{{ route('shop.edit', $response->id) }}">
         <img src="{{ asset('media/img/icons/edit.png') }}" alt="https://www.flaticon.es/iconos-gratis/editar Editar iconos creados por Kiranshastry">
+        </a>
     </div>
     @endif
 
@@ -21,7 +22,7 @@
 
 <div id="view-product">
     <div id="image-box">
-        <img id="product-img" src="{{" ../".$response->image }}">
+        <img id="product-img" src="{{asset($response->image) }}">
     </div>
 
     <div id="desc-box">
@@ -44,6 +45,7 @@
                 <input type="hidden" value="{{ $response->discount }}">
             </form>
             <span class="separate-price"></span>
+            @if($response->available == 1)
             <form action="{{ route('inventory.add')}}" method="POST">
                 @csrf
                 <div class="shop-now">
@@ -71,7 +73,16 @@
                 </button>
             </form>
             </div>
+            @else
+            <div class="shop-now">
 
+            <button class="btn btn-primary btn-simple add-cart-button">
+                <div class="cart-title">
+                    <p>Not available</p>
+                </div>
+
+            </div>
+            @endif
     </div>
 </div>
     </div>
